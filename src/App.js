a3a18357c93ff9429e32c9515fa54175e0d8b31e
Jaclyn-Foo <leeszefoo@gmail.com>
2022-06-15 00:55:54 +0800

    Added footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Calculator from './Calculator'
+import Footer from './Footer'
 
 export default function App() {
     return (
         <div id="app">
             <Calculator />
+            <Footer />
         </div>
     )
 }
---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <footer>
+            <address>Created by <a href="https://github.com/FooLeeSze/fcc-js_calculator" target="_blank" rel="noreferrer">Lee Sze Foo</a></address>
+        </footer>
+    )
+}
